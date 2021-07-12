d2d46a3d3be15cde086d6560364a228cad8ca00a
Ankan Sharma <ankanroni3@gmail.com>
2021-07-13 00:55:05 +0530

    inference class complete

--- Image Classification/inference.py
+++ Image Classification/inference.py
@@ -1,10 +1,54 @@
+import numpy as np
 import tensorflow as tf
 from arch.basic_convnet import model
-from data_src.data import MNIST
+import cv2
 
-def inference(x_test,y_test,model = model, model_path = '/tmp/ckpt'):
+'''
+0: T-shirt/top,
+1: Trouser,
+2: Pullover,
+3: Dress,
+4: Coat,
+5: Sandal,
+6: Shirt,
+7: Sneaker,
+8: Bag,
+9: Ankle boot,
+'''
 
+
+
+def inference(image,model = model, model_path = 'tmp/ckpt'):
+
+    map_class = {
+        0: "T-shirt/top",
+        1: "Trouser",
+        2: "Pullover",
+        3: "Dress",
+        4: "Coat",
+        5: "Sandal",
+        6: "Shirt",
+        7: "Sneaker",
+        8: "Bag",
+        9: "Ankle boot",
+    }
+
+    img = cv2.cvtColor(cv2.imread(image), cv2.COLOR_BGR2GRAY)
+    #print(img.shape)
+    img_nmp = cv2.resize(img,(28,28))
+    #print(img_nmp.shape)
+    img_nmp_reshaped = img_nmp.reshape((1,28,28,1))
     model_inference = model
     model_inference.load_weights(model_path)
 
-    y_pred = model_inference.evaluate(x_test)
+    y_pred = model_inference.predict(img_nmp_reshaped)
+    # maximum value's index from the numpy array y_pred
+    classified = np.argmax(y_pred, axis=1)
+    #print(map_class[classified[0]])
+    return map_class[classified[0]]
+
+
+if __name__ == '__main__':
+    image = r'C:\Users\Ankan\Desktop\Github\Fashion-MNIST\input_image\shirt.png'
+
+    object = inference(image,model = model,model_path = 'tmp/ckpt')
