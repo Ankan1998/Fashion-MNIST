1657e810a930cdc0ad34d698ea4d3bfe649378c0
Ankan Sharma <ankanroni3@gmail.com>
2021-07-12 22:40:07 +0530

    Data Loader Class Added

---
+++ Image Classification/data_src/data.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import numpy as np
+
+class MNIST:
+
+    def data_loader(self, path = 'mnist.npz'):
+
+        # Create MNIST dataset with dataloader
+        mnist = tf.keras.datasets.mnist
+        (x_train, y_train), (x_test, y_test) = mnist.load_data(path)
+
+        # Reshape training images to (60,000, 28, 28,1)
+        x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+        x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+
+        # Normalize input images from 0-255 to 0-1
+        x_train, x_test = x_train / 255, x_test / 255
+
+        return x_train, y_train, x_test, y_test
+
+
+if __name__ == '__main__':
+    mnist = MNIST()
+    x_train, y_train, x_test, y_test = mnist.data_loader('mnist.npz')
+    
+
+
+
+
+
