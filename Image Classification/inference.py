ac155419f1f7252ea0a81e4975c5b7e2f2547a73
Ankan Sharma <ankanroni3@gmail.com>
2021-07-13 00:56:13 +0530

    minor change

--- Image Classification/inference.py
+++ Image Classification/inference.py
@@ -3,20 +3,6 @@ import tensorflow as tf
 from arch.basic_convnet import model
 import cv2
 
-'''
-0: T-shirt/top,
-1: Trouser,
-2: Pullover,
-3: Dress,
-4: Coat,
-5: Sandal,
-6: Shirt,
-7: Sneaker,
-8: Bag,
-9: Ankle boot,
-'''
-
-
 
 def inference(image,model = model, model_path = 'tmp/ckpt'):
 
