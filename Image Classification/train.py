0ac4f9185ea5fe2f2fb0387279f26a30a4c64795
Ankan Sharma <ankanroni3@gmail.com>
2021-07-12 23:53:19 +0530

    Added checkpoint in training

--- Image Classification/train.py
+++ Image Classification/train.py
@@ -14,26 +14,34 @@ def training(x_train,
             loss='sparse_categorical_crossentropy',
             metrics=['accuracy'],
             batch_size=32,
-            epochs=25):
+            epochs=25,
+            ckt_path='/tmp/ckpt'):
 
     model.compile(optimizer=optimizer,loss=loss,metrics=metrics)
     model.summary()
+
+    model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=ckt_path,
+    save_weights_only=True,
+    monitor='val_accuracy',
+    mode='max',
+    save_best_only=True)
+
     model.fit(x_train,
             y_train, 
             batch_size = batch_size, 
             epochs=epochs,
             validation_data=(x_val,y_val),
-            shuffle=True)
+            shuffle=True,
+            callbacks=[model_checkpoint_callback])
 
-    score = model.evaluate(x_test,y_test,batch_size=batch_size)
-    print('Test score:',score[0])
-    print('Test accuracy:',score[1])
+
 
 
 if __name__ == '__main__':
     mnist = MNIST()
     x_train, y_train, x_val, y_val, x_test, y_test = mnist.data_loader('mnist.npz')
-    training(x_train, y_train, x_val, y_val, x_test, y_test)
+    training(x_train, y_train, x_val, y_val, x_test, y_test, epochs=1)
     
 
 
